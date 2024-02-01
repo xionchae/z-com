2ae507738dbc273f3d1eef51b0ce7caf5333b9ef
hcche <hcche@plateer.com>
2024-02-01 14:52:05 +0900

    섹션2 오른쪽 섹션 클론 코딩까지 route, modal, useSelectedLayoutSegment 등

---
+++ ch1-1/src/app/(afterLogin)/_component/TrendSection.tsx
@@ -0,0 +1,15 @@
+import style from './trendSection.module.css';
+import Trend from "@/app/(afterLogin)/_component/Trend";
+
+export default function TrendSection() {
+  return (
+    <div className={style.trendBg}>
+      <div className={style.trend}>
+        <h3>나를 위한 트렌드</h3>
+        <Trend />
+        <Trend />
+        <Trend />
+      </div>
+    </div>
+  )
+}
